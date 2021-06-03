f0f48ed2f7915418a9c423de9fafc4ecc9c822dc
Anthony Mogrovejo <tony001983@gmail.com>
2021-06-03 02:10:28 +0000

    Send Files Api

---
+++ api.py
@@ -0,0 +1,34 @@
+from flask import Flask
+app = Flask(__name__)
+from functools import wraps
+from flask import request, abort
+import ssl
+context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+context.load_cert_chain('server.crt', 'server.key')
+
+# The actual decorator function
+def require_appkey(view_function):
+    @wraps(view_function)
+    def decorated_function(*args, **kwargs):
+        with open('api.key', 'r') as apikey:
+            key=apikey.read().replace('\n', '')
+        if request.headers.get('x-api-key') and request.headers.get('x-api-key') == key:
+            return view_function(*args, **kwargs)
+        else:
+            abort(401)
+    return decorated_function
+
+@app.route('/devops/', methods=['POST'])
+@require_appkey
+def put_user():
+    return 
+    {"message" : "Hello Juan Perez your message will be send"}
+
+
+@app.route('/')
+def hello_world():
+    return 'Hola Mundo Prueba'
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=443, debug=True, ssl_context=context)
